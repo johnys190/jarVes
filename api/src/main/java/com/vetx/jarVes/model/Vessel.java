cc39d26a4d7a14761a00bf9229f90a8d5427ae59
thanos loukeris <myemailid@domain.com>
2018-12-19 16:23:37 +0200

    Voyestimate entinty not yet finished fixed repo and controller not yet finished

--- api/src/main/java/com/vetx/jarVes/model/Vessel.java
+++ api/src/main/java/com/vetx/jarVes/model/Vessel.java
@@ -1,6 +1,7 @@
 package com.vetx.jarVes.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Vessel {
 
     @Id
--- api/src/main/java/com/vetx/jarVes/repository/VoyEstimateRepository.java
+++ api/src/main/java/com/vetx/jarVes/repository/VoyEstimateRepository.java
@@ -1,11 +1,13 @@
 package com.vetx.jarVes.repository;
 
-import com.vetx.jarVes.model.Vessel;
+import com.vetx.jarVes.model.VoyEstimate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
 @CrossOrigin
-public interface VesselRepository extends JpaRepository<Vessel, Long> {
+public interface VoyEstimateRepository extends JpaRepository<VoyEstimate,Long> {
+
 }
+
