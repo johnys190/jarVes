e270f1a20bde34f1bb3266fcf2ce1935a7193c28
zantoichi <s.vallianos@acg.edu>
2018-12-24 22:15:25 +0200

    Adding basic structure for the impementation of an Entity-into-pdf delivered through an endpoint. Merged the VoyEstimate entity. Adding create and delete Vessel user endpoints.

--- api/src/main/java/com/vetx/jarVes/payload/SignUpRequest.java
+++ api/src/main/java/com/vetx/jarVes/payload/SignUpRequest.java
@@ -11,20 +11,21 @@ import javax.validation.constraints.Size;
 @Setter
 public class SignUpRequest {
 
-  @NotBlank
-  @Size(min = 4, max = 100)
-  private String firstName;
+    @NotBlank
+    @Size(min = 4, max = 100)
+    private String firstName;
 
-  @NotBlank
-  @Size(min = 3, max = 100)
-  private String lastName;
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String lastName;
 
-  @NotBlank
-  @Size(min = 6, max = 100)
-  private String password;
+    @NotBlank
+    @Size(min = 6, max = 100)
+    private String password;
 
-  @Email
-  @Size(max = 40)
-  @NotBlank
-  private String email;
+    @Email
+    @Size(max = 40)
+    @NotBlank
+    private String email;
+
 }
