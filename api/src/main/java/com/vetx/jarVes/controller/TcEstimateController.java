6f2caa937d731b7f266d644816ccea8c3c86a890
zantoichi <s.vallianos@acg.edu>
2018-12-26 21:22:03 +0200

    Added @Builder annotation to voyestimate and tcestimate entities and created some mock data for them

--- api/src/main/java/com/vetx/jarVes/controller/TcEstimateController.java
+++ api/src/main/java/com/vetx/jarVes/controller/TcEstimateController.java
@@ -3,17 +3,10 @@ package com.vetx.jarVes.controller;
 
 import com.vetx.jarVes.model.TcEstimate;
 import com.vetx.jarVes.repository.TcEstimateRepository;
-import com.vetx.jarVes.service.GeneratePDF;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import javax.validation.Valid;
 import java.util.List;
 
 @Api(description = "This is the Time Charter Estimate controller.")
@@ -34,23 +27,19 @@ public class TcEstimateController {
 
     @GetMapping("/{id}")
     //@PreAuthorize("hasRole('GUEST')")
-    public TcEstimate getEstimateById(@PathVariable Long id){
+    public TcEstimate getTcEstimateById(@PathVariable Long id){
         return tcEstimateRepository.findById(id).get();
     }
 
-//    @GetMapping(value = "/tcestimate-pdf", produces = MediaType.APPLICATION_PDF_VALUE)
-//    public ResponseEntity<InputStreamResource> customersReport() throws IOException {
-//        List<TcEstimate> tcEstimates = (List<TcEstimate>) tcEstimateRepository.findAll();
-//
-//        ByteArrayInputStream bis = GeneratePDF.voyEstimatePDF(tcEstimates);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add("Content-Disposition", "inline; filename=VOYestimate.pdf");
-//
-//        return ResponseEntity
-//                .ok()
-//                .headers(headers)
-//                .contentType(MediaType.APPLICATION_PDF)
-//                .body(new InputStreamResource(bis));
-//    }
+    @PutMapping("/{id}")
+    public TcEstimate updateTcEstimateById(@Valid @RequestBody TcEstimate updatedTcEstimate, @PathVariable Long id){
+//        TcEstimate updatedTcEstimate =  tcEstimateRepository.findById(updatedTcEstimate.getId());
+
+        return tcEstimateRepository.save(updatedTcEstimate);
+    }
+
+    @PostMapping()
+    public TcEstimate createTcEstimate(@Valid @RequestBody TcEstimate newTcEstimate){
+        return tcEstimateRepository.save(newTcEstimate);
+    }
 }
