b227c0d328440f0402a2676c53e967089af3b774
thanos loukeris <myemailid@domain.com>
2018-12-26 23:21:38 +0200

    fixed voyestimate controller fetch one delete put

--- api/src/main/java/com/vetx/jarVes/model/VoyEstimate.java
+++ api/src/main/java/com/vetx/jarVes/model/VoyEstimate.java
@@ -13,6 +13,7 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
+@AllArgsConstructor
 @Builder
 public class VoyEstimate {
     @Id
@@ -22,6 +23,7 @@ public class VoyEstimate {
     @Column(unique = true)
     private String name;
 
+    @NonNull
     private String voyage;
 
     private String account;
@@ -29,98 +31,98 @@ public class VoyEstimate {
     private String commodity;
 
     private String broker;
-
-    private String laycan;
-
-    private Double quantity;
-
-    //private Double Freightrate;
-
-    private Integer loadRate;
-
-    private Integer dischargeRate;
-
-    @Enumerated(EnumType.STRING)
-    private LayTimeType loadLayTimeType;
-
-    @Enumerated(EnumType.STRING)
-    private LayTimeType dischargeLayTimeType;
-
-    private Integer ballastDistanceSeca;
-
-    private Integer ballastDistanceNonSeca;
-
-    private Integer ladenDistanceSeca;
-
-    private Integer ladenDistanceNonSeca;
-
-    private Double commisionPercent;
-
-    private String reposition;
-
-    private Date date;
-
-    private Double ifoPrice;
-
-    private Double mdoPrice;
-
-    private Double lostWaitingDays;
-
-    private Double loadPortCosts;
-
-    private Double dischargePortCosts;
-
-    private Double othersPortCosts;
-
-    private Double canalsCost;
-
-    private Double taxesCosts;
-
-    private Double miscelCosts;
-
-    private Double exinsCosts;
-
-    private Double loadTimeLostPercent;
-
-    private Double disachargeTimeLostPercent;
-
-    private Double steamingMarginTimeLostPercent;
-
-    private Double steamingDays;
-
-    private Double loadDays;
-
-    private Double dischargeDays;
-
-    private Double shexLoadDays;
-
-    private Double shexDischargeDays;
-
-    private Double totalDurationDays;
-
-    private Double grossRevenueResult;
-
-    private Double sailingBunkersResult;
-
-    private Double loadPortBunkersResult;
-
-    private Double dischargePortBunkersResult;
-
-    private Double totalBunkerCostsResult;
-
-    private Double expensesResult;
-
-    private Double commisionsResult;
-
-    private Double taxesResult;
-
-    private Double exinsResult;
-
-    private Double netRevenueResult;
-
-    private Double timeCharterRateResult;
-
-    private Double sensitivity;
+//
+//    private String laycan;
+//
+//    private Double quantity;
+//
+//    //private Double Freightrate;
+//
+//    private Integer loadRate;
+//
+//    private Integer dischargeRate;
+//
+//    @Enumerated(EnumType.STRING)
+//    private LayTimeType loadLayTimeType;
+//
+//    @Enumerated(EnumType.STRING)
+//    private LayTimeType dischargeLayTimeType;
+//
+//    private Integer ballastDistanceSeca;
+//
+//    private Integer ballastDistanceNonSeca;
+//
+//    private Integer ladenDistanceSeca;
+//
+//    private Integer ladenDistanceNonSeca;
+//
+//    private Double commisionPercent;
+//
+//    private String reposition;
+//
+//    private Date date;
+//
+//    private Double ifoPrice;
+//
+//    private Double mdoPrice;
+//
+//    private Double lostWaitingDays;
+//
+//    private Double loadPortCosts;
+//
+//    private Double dischargePortCosts;
+//
+//    private Double othersPortCosts;
+//
+//    private Double canalsCost;
+//
+//    private Double taxesCosts;
+//
+//    private Double miscelCosts;
+//
+//    private Double exinsCosts;
+//
+//    private Double loadTimeLostPercent;
+//
+//    private Double disachargeTimeLostPercent;
+//
+//    private Double steamingMarginTimeLostPercent;
+//
+//    private Double steamingDays;
+//
+//    private Double loadDays;
+//
+//    private Double dischargeDays;
+//
+//    private Double shexLoadDays;
+//
+//    private Double shexDischargeDays;
+//
+//    private Double totalDurationDays;
+//
+//    private Double grossRevenue;
+//
+//    private Double sailingBunkers;
+//
+//    private Double loadPortBunkers;
+//
+//    private Double dischargePortBunkers;
+//
+//    private Double totalBunkerCosts;
+//
+//    private Double expenses;
+//
+//    private Double commisions;
+//
+//    private Double taxes;
+//
+//    private Double exins;
+//
+//    private Double netRevenue;
+//
+//    private Double timeCharterRate;
+//
+//    private Double sensitivity;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "Vessel_id")
